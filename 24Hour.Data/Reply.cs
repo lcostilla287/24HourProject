5a022e2f1364d852cc5537c4627ef85b5f272860
Luis Costilla <luis287@gmail.com>
2021-06-01 17:00:09 -0400

    reply properties added

---
+++ 24Hour.Data/Reply.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public class Reply
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
