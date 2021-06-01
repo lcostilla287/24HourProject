f96cf5c727d9f749e93a91891e9f2a9d575ea6f6
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-01 17:34:38 -0400

    added post class into data layer

---
+++ 24Hour.Data/Post.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Data
+{
+    public class Post
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public int CommentId { get; set; }
+        [ForeignKey(nameof(CommentId))]
+        public virtual Comment Comment { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+
+    }
+}
