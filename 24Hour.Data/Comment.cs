aa56bd28ec053fe6038452eed995a13b69064913
Frederick McClung <Frederick.McClung.SWDev@gmail.com>
2021-06-02 08:29:42 -0400

    Removed 'Required' from Text, AuthorId, CommentId, and virtual Reply

--- 24Hour.Data/Comment.cs
+++ 24Hour.Data/Comment.cs
@@ -13,14 +13,11 @@ namespace _24Hour.Data
         [Key]
         public int Id { get; set; }
 
-        [Required]
         public string Text { get; set; }
 
-        [Required]
         public Guid AuthorId { get; set; }
 
         [ForeignKey(nameof(Post))]
-        [Required]
         public int CommentId { get; set; }
         public virtual Reply Reply { get; set; }
 
