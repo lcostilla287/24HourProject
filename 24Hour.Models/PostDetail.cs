ffd73b62187e9eba5fef20e17adb73a789315663
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-02 09:37:17 -0400

    added post detail in models

---
+++ 24Hour.Models/PostDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Models
+{
+    class PostDetail
+    {
+        public int PostId { get; set; }
+        public int CommentId { get; set; }
+        public string Text { get; set; }
+
+    }
+}
