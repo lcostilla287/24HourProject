e4fab57f63c40b65f31c7a3be27d8b79268c0fec
JoelS-1 <82602257+JoelS-1@users.noreply.github.com>
2021-06-02 09:43:45 -0400

    added post create service

---
+++ 24Hour.Services/PostService.cs
@@ -0,0 +1,38 @@
+﻿using _24Hour.Data;
+using _24Hour.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24Hour.Services
+{
+    class PostService
+    {
+        private readonly Guid _userId;
+
+        public PostService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        //post
+
+        public bool CreatePost(PostCreate model)
+        {
+            var entity =
+                new Post()
+                {
+                    AuthorId = _userId,
+                    Title = model.Title,
+                    Text = model.Text
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Post.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+    }
+}
