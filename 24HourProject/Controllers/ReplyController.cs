ba603c1f3a830ca4c4ccc7cc55a97a262d8c6fab
Luis Costilla <luis287@gmail.com>
2021-06-02 09:47:43 -0400

    Reply controller with methods added

---
+++ 24HourProject/Controllers/ReplyController.cs
@@ -0,0 +1,44 @@
+﻿using _24Hour.Models;
+using _24Hour.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace _24HourProject.Controllers
+{
+    public class ReplyController : ApiController
+    {
+        private ReplyService CreateReplyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var replyService = new ReplyService(userId);
+            return replyService;
+        }
+
+        //Post
+        public IHttpActionResult Post(ReplyCreate reply)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateReplyService();
+
+            if (!service.CreateReply(reply))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        //Get
+        public IHttpActionResult GetRepliesByCommentId(int id)
+        {
+            ReplyService replyService = CreateReplyService();
+            var reply = replyService.GetRepliesByCommentId(id);
+            return Ok(reply);
+        }
+    }
+}
